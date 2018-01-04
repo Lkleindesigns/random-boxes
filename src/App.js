b68070e50d1186a7c596088b5ee1396fad84bec2
Logan Klein <lkleindesigns@gmail.com>
2018-01-04 05:10:28 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const NUM_RECT = 300;
+
+const Rect = ({color}) => {
+  const style = {
+    width: '100px',
+    height: '180px',
+    display: 'inline-block',
+    backgroundColor: color
+  }
+  return <div style={style} />
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const rects = Array(NUM_RECT).fill().map(this.getRandomColor, this)
+    this.state = {rects}
+    
+    setInterval(() => {
+      const rects = this.state.rects.slice()
+      const randIndex = Math.floor(Math.random() * rects.length)
+      rects[randIndex] = this.getRandomColor()
+      this.setState({rects})
+    },300)
+  }
+  
+
+  
+  getRandomColor() {
+    const colorIndex = Math.floor(Math.random() * this.props.allColors.length)
+    return this.props.allColors[colorIndex]
+  }
+ 
+  render() {
+    const rects = this.state.rects.map((color, i) => (
+    <Rect key={i} color={color} />
+    ))
+    return (
+     <div className="App">
+      {rects}
+     </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"]
+};
+
+export default App;
